66472ed801a9f5c1f282b905e1c0fda2661bf18b
YuryMylnikov <rabota.milo@gmail.com>
2017-07-10 13:55:25 +0300

    Lecture 11
    Немного оптимизировал.

--- Board.c
+++ Board.c
@@ -5,11 +5,11 @@ int main() {
 	int num = 8;
 	//-1 - серая клетка, 0 - белая
 	int board[8][8] = { { 0,  0, -1,  0, -1,  0,  0,  0},
-						{ 0, -1,  0,  0, -1, -1,  0,  0},
+						{ 0,  0, -1,  0,  0, -1,  0,  0},
 						{-1, -1,  0, -1,  0,  0,  0, -1},
 						{-1, -1,  0,  0,  0,  0,  0,  0},
 						{ 0, -1,  0,  0,  0, -1,  0,  0},
-						{ 0, -1,  0, -1, -1,  0, -1, -1},
+						{ 0, -1,  0, -1, -1,  0,  0, -1},
 						{ 0,  0,  0,  0,  0,  0,  0,  0},
 						{ 0,  0, -1,  0,  0, -1,  0, -1} };
 
@@ -22,31 +22,31 @@ int main() {
 
 	{
 		//Определение веса каждой клетки
-		int cellWeight = 0;
 		for (int i = 0; i < num; i++)
-			for (int j = 0; j < num; ) {
+			for (int j = num - 1; j >= 0; ) {
+				int ind = j - 1;
 				if (board[i][j] == 0) {
-					cellWeight = 0;
-					int ind = j;
-					while (ind < num && board[i][ind] == 0) {//от текущего j до первой серой клетки или конца строки
-						cellWeight++;
-						ind++;
+					board[i][j] = 1;
+					while (ind >= 0 && board[i][ind] == 0) {//от текущего j до первой серой клетки или конца строки
+						board[i][ind] = board[i][ind + 1] + 1;
+						ind--;
 					}
-
-					board[i][j] = cellWeight;//первой белой клетке ряда присваиваем её вес
-					for (ind = j + 1; ind < j + cellWeight; ind++)//каждой последующей клетке белого ряда присваиваем её вес
-						board[i][ind] = board[i][ind - 1] - 1;
 				}
 
 				//определяем слудующий j
-				if (cellWeight == 0)
-					j++;
-				else {
-					j += cellWeight;
-					cellWeight = 0;
-				}
-			}
+				if (ind == j - 1)
+					j--;
+				else 
+					j = ind - 1;
+			}//for (int j = num - 1; j >= 0; )
 	}
+
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j < num; j++)
+			printf("%3d", board[i][j]);
+		printf("\n");
+	}
+	printf("\n");
 
 	{
 		//Вычисление площади прямоугольников
@@ -60,7 +60,7 @@ int main() {
 					int ind = i;
 					while (ind < num && board[ind][j] > 0) {//пока не конец столбца и клетка не серая
 						int curSquare = cellsLine*(ind - i + 1);
-						if ((board[ind + 1][j] < 0 || cellsLine > board[ind + 1][j]) && curSquare > square) {
+						if ((board[ind + 1][j] < 0 || cellsLine > board[ind + 1][j] || ind >= num - 1) && curSquare > square) {
 							square = curSquare;
 							topLeft[0] = i;
 							topLeft[1] = j;
